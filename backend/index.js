0b59a39750ab42c7f0d8e9ee548983a6f52c49a1
lasith <lasithamanda1999@gmail.com>
2024-02-29 00:06:01 +0530

    create a book

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 //add express.js frameworks
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDbUrl } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookmodel.js";
 
 const app = express();
 
@@ -9,6 +10,38 @@ const app = express();
 app.get('/', (request,response) => {
     console.log(request);
     return response.status(234).send('Welcome To Book Store');
+});
+
+// Save a new book
+app.post('/books', async (request, response) => {
+    //for success and failure
+    try {
+      //validation for input
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            request.body.publishYear
+        ) {
+            //if response 400 return message for client
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        //new book variable
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        //book upgrade
+        const book = await Book.create(newBook);
+        //return status 201 and book to client
+        return response.status(201).send(book);
+
+      } catch (error) {//receive error
+        console.log(error.message);//error to server console
+        response.status(500).send({ message: error.message });//return response
+    }
 });
 
 
---
+++ backend/models/bookmodel.js
@@ -0,0 +1,26 @@
+//create book schema
+import mongoose from "mongoose";
+
+//create book schema variable
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+//create model
+export const Book = mongoose.model('Cat', { name: String });
